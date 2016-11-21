8caee48100718d74ab53d564f93810801c9a7651
Zehad <nazrulcse6284@gmail.com>
2016-11-21 20:05:35 +0600

    first commit

---
+++ app/src/main/java/com/nazrul/profilescreen/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nazrul.profilescreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.material_design_profile_screen_xml_ui_design);
+    }
+}
